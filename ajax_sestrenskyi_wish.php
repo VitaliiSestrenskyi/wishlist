fd697239a86f41cbbfbddf7c399bd91b15f48364
VitaliiSestrenskyi <sestrinskiy@mail.ru>
2016-04-20 11:51:48 +0300

    new D7

--- ajax_sestrenskyi_wish.php
+++ ajax_sestrenskyi_wish.php
@@ -1,25 +1,31 @@
-<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+<?php 
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+define('ITUA_WISH_LIST', 4);
 
-$idProduct = trim($_POST['idProduct']);
-$idUser = trim($_POST['idUser']);
-
-CModule::IncludeModule("highloadblock"); 
 use Bitrix\Highloadblock as HL; 
 use Bitrix\Main\Entity; 
+use Bitrix\Main\Loader;
+use Bitrix\Main\Application;
+use Bitrix\Main\SystemException; 
 
-$hlbl = 7;
-$hlblock = HL\HighloadBlockTable::getById($hlbl)->fetch(); 
-// get entity 
-$entity = HL\HighloadBlockTable::compileEntity($hlblock); 
-$entity_data_class = $entity->getDataClass();  
-if(isset($idProduct) && isset($idUser)){
-	$data = array(
-				 "UF_PRODUCT" => $idProduct,	
-				 "UF_USER" => $idUser
-				 );	
+Loader::includeModule('highloadblock');
+$request = Application::getInstance()->getContext()->getRequest(); 
+
+$idProduct  = (int)$request->getPost('idProduct');
+$idUser 	= (int)$request->getPost('idUser'); 
+
+if( $idProduct && $idUser ){
+	$hlblock = HL\HighloadBlockTable::getById(ITUA_WISH_LIST)->fetch(); 
+	$entity  = HL\HighloadBlockTable::compileEntity($hlblock); 
+	$entity_data_class = $entity->getDataClass();  
+	if(isset($idProduct) && isset($idUser)){
+		$data = array(
+					 "UF_PRODUCT" => $idProduct,	
+					 "UF_USER" => $idUser
+					 );	
+	}
+	$result = $entity_data_class::add($data);
+}else{
+	throw new SystemException("Error with parameters - idProduct and idUser"); 
 }
-$result = $entity_data_class::add($data);
-
-
 ?>    
-    
